60e1ae4832b19fb838f9e9d7dd54f988279b89ae
YashSrivastava26 <yashsrivastava2603@gmail.com>
2023-12-25 21:42:42 +0530

    refactor:logo size and metadata

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -13,15 +13,15 @@ interface LoaderProps {}
 const Loader: FC<LoaderProps> = ({}) => {
   return (
     <div className="h-full w-full min-h-screen flex flex-col justify-center items-center gap-4">
-      <Logo className="h-auto w-[15%]" />
+      <Logo className="h-auto 2xl:w-[15%] xl:w-[20%] lg:w-[25%] md:w-[30%] sm:w-[35%] w-[40%]" />
 
-      <div className="flex items-center justify-center text-muted-foreground text-base gap-8 relative">
+      <div className="flex items-center justify-center text-muted-foreground text-base gap-8 relative text-center">
         <p>Preparing my journey. . . Are you ready?</p>
       </div>
 
       <div>
         <h1 className="hero-text text-center m-4">Check these out</h1>
-        <div className="flex items-center justify-center gap-x-8">
+        <div className="flex items-center justify-center gap-x-6 md:gap-x-8">
           <AttachmentTooltip animate className="h-8 w-8 text-primary/80" />
 
           <TooltipProvider>
