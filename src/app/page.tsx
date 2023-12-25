e8a1cc81b2dc479f63b5c7da6865192882dc77a4
YashSrivastava26 <yashsrivastava2603@gmail.com>
2023-12-25 11:21:54 +0530

    refactor:lockIcon size, loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,6 @@
-import Loader from "@/components/Loader";
 import dynamic from "next/dynamic";
 export default function Home() {
-  const Main = dynamic(() => import("../components/main"), {
-    ssr: false,
-    loading: () => (
-      <div className="z-50 fixed flex items-center justify-center h-screen w-screen">
-        <Loader />
-      </div>
-    ),
-  });
+  const Main = dynamic(() => import("../components/main"));
   return (
     <main className="flex flex-col items-center justify-center h-full w-full min-h-screen">
       <Main />
--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,4 +1,5 @@
-import { FC } from "react";
+"use client";
+import { FC, useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import Attachments from "./Attachments/Attachments";
 import Hero from "./Hero/Hero";
@@ -7,22 +8,37 @@ import Project from "./Project";
 import Skills from "./Skills/Skills";
 import StarsCanvas from "./Models3D/StarsCanvas";
 import Contact from "./Contact";
+import Loader from "./Loader";
 
 interface MainProps {}
 
 const Main: FC<MainProps> = ({}) => {
+  const [showLoader, setShowLoader] = useState<boolean>(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowLoader(false);
+    }, 4500);
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <>
-      <Navbar />
-      <Attachments />
-      <div className="flex flex-col items-center gap-20 w-screen max-w-7xl overflow-x-hidden">
-        <Hero />
-        <Encryption />
-        <Project />
-        <Skills />
-        <Contact />
-      </div>
-      <StarsCanvas />
+      {showLoader ? (
+        <Loader />
+      ) : (
+        <>
+          <Navbar />
+          <Attachments />
+          <div className="flex flex-col items-center gap-20 w-screen max-w-7xl overflow-x-hidden">
+            <Hero />
+            <Encryption />
+            <Project />
+            <Skills />
+            <Contact />
+          </div>
+          <StarsCanvas />
+        </>
+      )}
     </>
   );
 };
